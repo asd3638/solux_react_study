bf01e16ddde4ed88f334327c3003d737d8c7ab75
jihye <asd36384@gmail.com>
2021-06-18 23:52:41 +0900

    0618

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
 
-function App() {
+function UserInfo(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="UserInfo">
+      <img className="Avatar"
+        src={props.user.avatarUrl}
+        alt={props.user.name}
+      />
+      <div className="UserInfo-name">
+        {props.user.name}
+      </div>
     </div>
   );
 }
+function formatDate(date) {
+  return date.toLocaleDateString();
+}
 
+function Comment(props) {
+  return (
+    <div className="Comment">
+      <UserInfo user={props.author}></UserInfo>
+      <div className="Comment-text">
+        {props.text}
+      </div>
+      <div className="Comment-date">
+        {formatDate(props.date)}
+      </div>
+    </div>
+  );
+}
+const comment = {
+  date: new Date(),
+  text: '리액트 0618 공부 인증',
+  author: {
+    name: '김지혜',
+    avatarUrl: 'https://placekitten.com/g/64/64',
+  },
+};
+
+function App() {
+  return (
+    <Comment
+    date={comment.date}
+    text={comment.text}
+    author={comment.author}
+    />
+  );
+}
 export default App;
